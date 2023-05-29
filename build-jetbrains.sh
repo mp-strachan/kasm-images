d388339e6d0bc4cfce29a6529568c19ca6b3f9cd
root <root@srv02.saturn-six.lab>
2023-05-29 19:49:40 +1000

    update build script

--- build-jetbrains.sh
+++ build-jetbrains.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-WEBSTORM_VERSION = 2023.2.1
-
+WEBSTORM_VERSION=2023.2.1
 
 # Webstorm
-echo "Building Webstorm ${WEBSTORM_VERSION}..."
-#docker build --build-arg var_name=${WEBSTORM_VERSION}
+echo "Building Webstorm $WEBSTORM_VERSION..."
+sudo docker build -t webstorm:$WEBSTORM_VERSION --build-arg version=$WEBSTORM_VERSION -f dockerfile-webstorm .
