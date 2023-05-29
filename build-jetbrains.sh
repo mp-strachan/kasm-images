f8a1998a02bd86739ec0f105a3fb1c4fa4d55504
Matthew Strachan <matthew.strachan@greatbigevents.com>
2023-05-29 19:53:31 +1000

    Updated to Webstorm 2023.1.2

--- build-jetbrains.sh
+++ build-jetbrains.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-WEBSTORM_VERSION=2023.2.1
+WEBSTORM_VERSION=2023.1.2
 
 # Webstorm
 echo "Building Webstorm $WEBSTORM_VERSION..."
-sudo docker build -t webstorm:$WEBSTORM_VERSION --build-arg version=$WEBSTORM_VERSION -f dockerfile-webstorm .
+sudo docker build -t webstorm:$WEBSTORM_VERSION --build-arg WEBSTORM_VERSION=$WEBSTORM_VERSION -f dockerfile-webstorm .
